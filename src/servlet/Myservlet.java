b3d44b774235f1516bfebcd7d2218bb857544d9b
ljm <1307674650@qq.com>
2018-12-04 11:04:35 +0800

    create myweb 00

---
+++ src/servlet/Myservlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/myservlet")
+public class Myservlet extends HttpServlet {
+	
+	
+	private static final long serialVersionUID = 1L;
+       
+    public Myservlet() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		response.getWriter().print("get∑√Œ ∑Ω Ω");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+
+}
